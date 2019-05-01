72fa95d5b9dac0aa03de926679a19f61503ecaba
Ryan Shackleton <ryshack@uw.edu>
2019-05-01 12:28:43 -0700

    Deploy static version of storybook.

---
+++ docs/storybook/precache-manifest.783ff153a485010f4f59339c8f0048b0.js
@@ -0,0 +1,15 @@
+self.__precacheManifest = [
+  {
+    "url": "main.0ac4be6ec60fff206393.bundle.js"
+  },
+  {
+    "url": "runtime~main.0ac4be6ec60fff206393.bundle.js"
+  },
+  {
+    "url": "vendors~main.0ac4be6ec60fff206393.bundle.js"
+  },
+  {
+    "revision": "dc63c1ca6ecdb372585d4b72d92f9a37",
+    "url": "iframe.html"
+  }
+];
