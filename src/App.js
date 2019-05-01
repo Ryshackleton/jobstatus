305c9a5640735a59c27869761f8f26f310cd1c35
Ryan Shackleton <ryshack@uw.edu>
2019-04-30 21:33:11 -0700

    Validate nNodes param before rendering

--- src/App.js
+++ src/App.js
@@ -5,26 +5,28 @@ import JobStatus from './containers/JobStatus/JobStatus';
 
 const JobStatusDemoWithNNodes = ({ match }) => {
   const nNodes = match.params.nNodes ? Number(match.params.nNodes) : 1000;
-  return <div className="App">
-    <h2 className="Job-status-title">Job Status Monitor for {nNodes} nodes</h2>
-    <div className="Job-status-graph-container">
-      <JobStatus
-        className="Job-status-graph"
-        labelFont="700 1.1rem Nunito"
-        labelFontSecondary = "700 0.9rem Nunito"
-        margin={{ top: 60, right: 20, bottom: 20, left: 20 }}
-        nNodes={nNodes}
-        pollingInterval={500}
-        wrapperStyle={{ height: '80%', width: '80%' }}
-      />
-    </div>
-  </div>
+  return isNaN(nNodes)
+    ? <div/>
+    : <div className="App">
+      <h2 className="Job-status-title">Job Status Monitor for {nNodes} nodes</h2>
+      <div className="Job-status-graph-container">
+        <JobStatus
+          className="Job-status-graph"
+          labelFont="700 1.1rem Nunito"
+          labelFontSecondary = "700 0.9rem Nunito"
+          margin={{ top: 60, right: 20, bottom: 20, left: 20 }}
+          nNodes={nNodes}
+          pollingInterval={500}
+          wrapperStyle={{ height: '80%', width: '80%' }}
+        />
+      </div>
+    </div>;
 };
 
 export default function App() {
   return (
     <Router>
-      {["/jobstatus/:nNodes", "/jobstatus/", "/:nNodes", ""].map((path, i) => (
+      {["/jobstatus/:nNodes", "/jobstatus", "/:nNodes", ""].map((path, i) => (
         <Route
           key={i}
           path={path}
