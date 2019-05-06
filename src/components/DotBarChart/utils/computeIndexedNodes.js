1f514c9b4a7f3dfae198e4d65f893af2e4445f36
Ryan Shackleton <ryshack@uw.edu>
2019-05-05 22:26:16 -0700

    When indexing nodes, also compute a unique color, which will serve as a lookup for mouse events.

--- src/components/DotBarChart/utils/computeIndexedNodes.js
+++ src/components/DotBarChart/utils/computeIndexedNodes.js
@@ -1,5 +1,11 @@
 import { reduce } from 'lodash-es';
 
+const uniqueColor = (index) => {
+  return (index < 16777215)
+    ? `rgb(${index & 0xff},${(index & 0xff00) >> 8},${(index & 0xff0000) >> 16})`
+    : '';
+};
+
 export default ({
   nodes,
   uniqueIdPropName = 'uniqueId',
@@ -11,12 +17,20 @@ export default ({
     ? groupKeys.reduce((acc, key) => ({ ...acc, [key]: 0 }), {})
     : {};
 
+  const colorToNode = {};
+  const nodeIdToColor = {};
   const indexed = reduce(nodes, (acc, node, index) => {
     const { [groupPropName]: group, [uniqueIdPropName]: id = index, } = node;
     if(!groupCounts[group]) {
       groupCounts[group] = 0;
     }
     const groupIndex = groupCounts[group]++;
+
+    // unique color (from http://bl.ocks.org/Jverma/70f7975a72358e6d69cdd4bf6a0569e7)
+    const color = uniqueColor(index + 1);
+    colorToNode[color] = node;
+    nodeIdToColor[id] = color;
+
     acc[id] = {
       ...node,
       [uniqueIdPropName]: id,
@@ -25,5 +39,5 @@ export default ({
     };
     return acc;
   }, {});
-  return { nodes: indexed, groupCounts, groupIndexPropName };
+  return { colorToNode, nodes: indexed, groupCounts, groupIndexPropName, nodeIdToColor };
 };
