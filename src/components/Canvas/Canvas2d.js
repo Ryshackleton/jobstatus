07fc65302eab7b6ea244dffe475e997e06607174
Ryan Shackleton <ryshack@uw.edu>
2019-05-05 22:24:23 -0700

    Add an optional second canvas to ResponsiveCanvas to capture touch/mouse events

--- src/components/Canvas/Canvas2d.js
+++ src/components/Canvas/Canvas2d.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import  useCanvas from '../../hooks/useCanvas';
 
-export default ({
+export default function Canvas2d({
+  click,
   drawFunction = () => {},
   height = 500,
   width = 500,
   canvasPixelRatio = 2,
+  mousedown,
+  mousemove,
+  mouseup,
   style = {},
-}) => {
+  touchend,
+  touchmove,
+  touchstart,
+}) {
   const canvasRef = useCanvas(drawFunction, canvasPixelRatio);
   const sizeInPixels = { width: `${width}px`, height: `${height}px` };
   return <canvas
@@ -15,6 +22,13 @@ export default ({
     height={height * canvasPixelRatio}
     style={{ ...sizeInPixels, ...style }}
     ref={canvasRef}
+    onTouchStart={touchstart}
+    onTouchMove={touchmove}
+    onTouchEnd={touchend}
+    onMouseMove={mousemove}
+    onMouseDown={mousedown}
+    onMouseUp={mouseup}
+    onClick={click}
   />;
 }
 
