296e3e806b7f8f225cc797386a7d9a7ec33dee0e
Ryan Shackleton <ryshack@uw.edu>
2019-06-23 20:45:43 -0700

    Move JobStatusDemoWithNodes to its own component named DemoWorkflowStatus and add a new component ApiJobStatus that pulls from API routes specified with env variables.

--- src/App.js
+++ src/App.js
@@ -1,50 +1,41 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import JobStatus from './containers/JobStatus/JobStatus';
-import { getDemoMetaDataAndNodes } from './containers/JobStatus/demoDataGenerator';
+import ApiJobStatus from './containers/ApiJobStatus/ApiJobStatus';
+import DemoWorkflowStatus from './containers/DemoWorkflowStatus/DemoWorkflowStatus';
 
-
-const JobStatusDemoWithNNodes = ({ match }) => {
-  const nNodes = match.params.nNodes ? Number(match.params.nNodes) : 1000;
-  const groupPropName = 'group_id';
-  /** nodesMetadataPromise should resolve to an object with properties:
-   * {
-   *  groupMetadataArray - array of objects with properties colorPropName, groupPropName, and labelPropName
-   *  nodes - array of objects with colorPropName, groupPropName and (optional) uniqueIdPropName
-   * }
-  */
-  const nodesMetadataPromise = getDemoMetaDataAndNodes.bind(null, { nNodes, groupPropName });
-
-  return <div className="App">
-    <h2 className="Job-status-title">Job Status Monitor for {nNodes} nodes</h2>
-    <div className="Job-status-graph-container">
-      <JobStatus
-        colorPropName="color"
-        className="Job-status-graph"
-        groupPropName={groupPropName}
-        labelFont="700 1.1rem Nunito"
-        labelFontSecondary = "700 0.9rem Nunito"
-        labelPropName="label"
-        margin={{ top: 60, right: 20, bottom: 20, left: 20 }}
-        nodesMetadataPromise={nodesMetadataPromise}
-        pollingInterval={1500}
-        uniqueIdPropName="demoDataGeneratorId"
-        wrapperStyle={{ height: '80%', width: '80%' }}
-      />
-    </div>
-  </div>;
-};
-
-export default function App() {
+export default function App({
+  baseUrl = process.env.REACT_APP_API_BASE_URL,
+  port = process.env.REACT_APP_API_PORT,
+  routeName = process.env.REACT_APP_API_JOB_STATUS_ROUTE_NAME
+}) {
   return (
     <Router>
       <Switch>
-        {["/jobstatus/:nNodes", "/jobstatus", "/:nNodes", ""].map((path, i) => (
+        {[`/demo/:nNodes`, "/demo"].map((path) => (
+          <Route
+            key="demo"
+            exact path={path}
+            render={(matchProps) => (
+              <DemoWorkflowStatus
+                {...matchProps} // contains params.match.nNodes
+                className="App"
+              />
+            )}
+          />
+        ))}
+        {[`/${routeName}/:job_id`, ""].map((path, i) => (
           <Route
             key={i}
             exact path={path}
-            component={JobStatusDemoWithNNodes}
+            render={(matchProps) => (
+              <ApiJobStatus
+                { ...matchProps } // contains params.match.job_id
+                className="App"
+                baseUrl={baseUrl}
+                port={port}
+              />
+            )}
           />
         ))}
       </Switch>
